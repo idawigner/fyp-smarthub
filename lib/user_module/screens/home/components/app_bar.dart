1b893623ec7527cb808a437f04ff45ce55ca6716
Anamshahid12 <shahidanam127@gmail.com>
2023-06-11 19:07:39 -0700

    abc

---
+++ lib/user_module/screens/home/components/app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:smarthub/constant.dart';
+
+AppBar homeAppBar(BuildContext context) {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    leading: IconButton(
+      icon: SvgPicture.asset("assets/icons/menu.svg"),
+      onPressed: () {},
+    ),
+    title: RichText(
+      textAlign: TextAlign.justify,
+      text: TextSpan(
+        style: Theme.of(context)
+            .textTheme
+            .titleMedium
+            ?.copyWith(fontWeight: FontWeight.bold),
+        children: const [
+          TextSpan(
+            text: "Smart",
+            style: TextStyle(color: yPrimaryColor),
+          ),
+          TextSpan(
+            text: "Hub",
+            style: TextStyle(color: rPrimaryColor),
+          ),
+        ],
+      ),
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: SvgPicture.asset("assets/icons/notification.svg"),
+        onPressed: () {},
+      ),
+    ],
+  );
+}
